0105595b44697d0a501037f09421a88f5b997d45
Jonathan <thekenau@gmail.com>
2026-03-16 02:58:25 -0600

    second commit

--- backend/routes/cities.js
+++ backend/routes/cities.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const cityModel = require('../model/cityModel');
 
-// Ruta de prueba
-router.get('/test', (req, res) => {
-  res.send({ msg: 'Cities test route.' })
+router.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+/*get all cities*/
+router.get('/all', (req, res) => {
+  cityModel.find()
+    .then(files => {
+      res.send(files);
+    })
+    .catch(err => console.log(err));
+});
+
+/*post city*/
+router.post('/', (req, res) => {
+  const nuevaCiudad = new cityModel({
+    name: req.body.name,
+    country: req.body.country
+  });
+
+  nuevaCiudad.save()
+    .then(ciudad => {
+      res.send(ciudad);
+    })
+    .catch(err => {
+      res.status(500).send("Server error");
+    });
 });
 
 module.exports = router;
