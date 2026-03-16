a05cf7169d34366aa23569a34a1606d1148dd0fb
Jonathan <thekenau@gmail.com>
2026-03-16 03:08:53 -0600

    citymodel

--- backend/model/cityModel.js
+++ backend/model/cityModel.js
@@ -1,18 +1,10 @@
 const mongoose = require('mongoose');
 
 const citySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  country: {
-    type: String,
-    required: true
-  },
-  img: {
-    type: String
-  }
+  name: { type: String, required: true },
+  country: { type: String, required: true },
+  img: { type: String }
 });
 
-module.exports = mongoose.model('city', citySchema);
+
+module.exports = mongoose.model('city', citySchema, 'cities');
